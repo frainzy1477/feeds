c78b07855fc5b246e6b9058e6d0e057fcd9d72ce
pexcn <i@pexcn.me>
2019-08-13 14:08:29 +0800

    refine code

--- compile.sh
+++ compile.sh
@@ -8,23 +8,28 @@ function get_sources() {
   mv $(ls -1 | grep $TARGET) $SDK_DIR
 
   cd $SDK_DIR
-  git clone --single-branch -b master https://github.com/openwrt-dev/feeds.git package/custom && cd package/custom
-  git submodule update --init --recursive
+  git clone --single-branch -b master --recurse-submodules -j4 https://github.com/openwrt-dev/feeds.git package/feeds
   cd $CUR_DIR
 }
 
-function compile_packages() {
+function build_packages() {
   cd $SDK_DIR
 
   make defconfig
   make prereq
+
+  # prepare
   make package/libev/compile V=w
   make package/libcares/compile V=w
   make package/libsodium/compile V=w
   make package/mbedtls/compile V=w
   make package/pcre/compile V=w
+
+  # luci
   make package/luci-app-shadowsocks/compile V=w
   make package/openwrt-dist-luci/compile V=w
+
+  # core
   make package/openwrt-chinadns/compile V=w
   make package/openwrt-cleandns/compile V=w
   make package/openwrt-dns-forwarder/compile V=w
@@ -43,7 +48,8 @@ function compile_packages() {
   make package/openwrt-simple-obfs/compile V=w \
     CONFIG_SIMPLE_OBFS_STATIC_LINK=y
 
-  ./staging_dir/host/bin/usign -G -s ./key-build -p ./key-build.pub -c "OpenWRT feeds build key"
+  # sign
+  ./staging_dir/host/bin/usign -G -s ./key-build -p ./key-build.pub -c "OpenWrt feeds build key"
   make package/index V=s
 
   cd $CUR_DIR
@@ -55,5 +61,5 @@ function dist_release() {
 }
 
 get_sources
-compile_packages
+build_packages
 dist_release
